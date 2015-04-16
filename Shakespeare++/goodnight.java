5a230d323140ad739edc592866d36007f1d92023
Gregory Hosono <ghosono@gmail.com>
2015-04-15 22:01:23 -0700

    Adds Shakespeare++

---
+++ Shakespeare++/goodnight.java
@@ -0,0 +1,9 @@
+//Play: Romeo and Juliet
+//Character: Juliet
+//Line: "A thousand times good night!"
+
+
+while (count < 1000) {
+	goodnight();
+	count++;
+}
---
+++ Shakespeare++/toBeOrNotToBe.java
@@ -0,0 +1,6 @@
+//Play: Hamlet
+//Character: Hamlet
+//Line: "To be or not to be"
+
+
+toBe || !toBe
