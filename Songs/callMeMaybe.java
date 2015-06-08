decd16df2c42b9a1ac4507efe401e6b31aa6181b
Gregory Hosono <ghosono@gmail.com>
2015-06-08 00:22:59 -0700

    adds Call Me Maybe in Java

---
+++ Songs/callMeMaybe.java
@@ -0,0 +1,33 @@
+/**
+* Title: Call Me Maybe in Java
+* To: Everyone taking AP Computer Science
+* From: @ghosono
+*/
+
+public class CallMeMaybe
+{
+	public static String myNumber = "(650) 515-3355";
+	public static String maybe = "So call me maybe!";
+	public static String maybeNot = "Before you came into my life, I missed you so bad. And you should know that";
+
+	public static void main (String[] args)
+	{
+		System.out.print(callMeMaybe(true, true));
+	}
+
+	public static String callMeMaybe(boolean justMetYou, boolean thisIsCrazy)
+	{
+		if (justMetYou && thisIsCrazy)
+		{
+			System.out.println("Hey! I just met you! And this is crazy!");
+			System.out.print("But here's my number: " + myNumber);
+			System.out.println("");
+			return maybe;
+		}
+		else
+		{
+			return maybeNot;
+
+		}
+	}
+}
