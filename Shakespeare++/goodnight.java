432ba74dcc086dc722830e01c89918c3940306a3
Gregory Hosono <ghosono@gmail.com>
2015-11-22 22:52:57 -0800

    Removes Shakespeare++ and songs

--- Shakespeare++/goodnight.java
+++
@@ -1,9 +0,0 @@
-//Play: Romeo and Juliet
-//Character: Juliet
-//Line: "A thousand times good night!"
-
-
-while (count < 1000) {
-	goodnight();
-	count++;
-}
--- Shakespeare++/toBeOrNotToBe.java
+++
@@ -1,6 +0,0 @@
-//Play: Hamlet
-//Character: Hamlet
-//Line: "To be or not to be"
-
-
-toBe || !toBe
